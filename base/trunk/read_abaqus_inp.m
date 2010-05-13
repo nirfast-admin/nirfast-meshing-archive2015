1aaf1d8e6914ee74374a75444316d741cab4edd6
hamiid <hamiid@fb8e7f84-bfd6-7a22-e4a7-981ca257bf3e>
2010-05-13 21:52:45 +0000

    added read_abaqus_inp_3D (which is indentical to read_abaqus_inp) and changed content of read_abaqus_inp accordingly.

--- base/trunk/read_abaqus_inp.m
+++ base/trunk/read_abaqus_inp.m
@@ -5,26 +5,7 @@ function [elem, points] = read_abaqus_inp(mesh_fn)
 % The old version which is very slow is now called read_abaqus_inp_line
 % 
 
-if(nargin~=1)
-    [fname,pname]=uigetfile('*.inp','Please pick the inp file');
-else
-    pname = [];
-	fname=mesh_fn;
-end
-[fid st]=OpenFile([pname fname],'r');
-if st~=0, error(' '); end
+% For compatibility reason with nirfast suit, we call the identical function whose
+% name has an extra '3D' in it
 
-% read header junk
-for i=1:4, fgetl(fid); end
-% read node coordinates
-data=textscan(fid,' %u64, %.12f, %.12f, %.12f%*[^\n]');
-points = [data{2} data{3} data{4}];
-
-% read head junk
-fgetl(fid);
-
-% read element list
-data=textscan(fid,' %u64, %u64, %u64, %u64%*[^\n]');
-elem  = double([data{2} data{3} data{4}]);
-
-fclose(fid);
+[elem, points] = read_abaqus_inp_3D(mesh_fn)
