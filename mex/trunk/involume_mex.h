ad5199cb9a9298ba208ac2c5ae02f3c7e1db492b
hamiid <hamiid@fb8e7f84-bfd6-7a22-e4a7-981ca257bf3e>
2010-05-29 00:11:39 +0000

    added involume_mex and tag_checkerboard3d_mex C++ files

---
+++ mex/trunk/involume_mex.h
@@ -0,0 +1,10 @@
+#ifndef __involume_mex_h
+#define __involume_mex_h
+
+#include <cmath>
+#include "mex.h"
+
+#include "isinvolume_randRay.h"
+
+
+#endif
---
+++ mex/trunk/tag_checkerboard3d_mex.h
@@ -0,0 +1,51 @@
+#ifndef tag_checkerboard3d_mex_h
+#define tag_checkerboard3d_mex_h
+
+//#define _mydebug
+
+#if defined(__APPLE__) || defined(OSX) || defined(__MACH__) || defined(linux) || defined(__linux)
+#include <sys/resource.h>
+#include <unistd.h>
+#endif
+ 
+ 
+#include <math.h>
+#include <time.h>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+#include "mex.h"
+#include "CStopWatch.h"
+
+typedef unsigned long ULONG;
+
+#define outside 5
+#define inside 6
+#define NA 3
+#define node_code 2
+#define boundary_node_code 1
+#define on_facet 7
+
+#if defined(WIN32) || defined(WIN64)
+#define _stupidMS_min _cpp_min
+#define _stupidMS_max _cpp_max
+#define round(x) ((int)((x) > 0.0 ? (x) + 0.5 : (x) - 0.5))
+#else
+#define _stupidMS_min std::min
+#define _stupidMS_max std::max
+#endif
+
+#ifndef myrand
+#define myrand(n) ( (long)  ( (double)rand() / ((double)(RAND_MAX)+(double)(1.0)) * (double)n ) )
+#endif
+
+inline bool ReadyForTermination(int *row_state, int& nrow, int& ncol, int& npln);
+
+struct mypoint {
+	double coords[3];
+};
+
+#endif
+
