47a60babf6590b83d3c0fef808087449cb03ab36
hamiid <hamiid@fb8e7f84-bfd6-7a22-e4a7-981ca257bf3e>
2010-06-21 05:31:36 +0000

    Improved performance of checking edges of a triangular mesh by using Matlab builtin methods. Improved CheckMesh3D in the way it handles information back from other subroutines. checkmesh3d_surface now uses a standalone checkedge routine.

--- base/trunk/CheckMesh3D.m
+++ base/trunk/CheckMesh3D.m
@@ -1,4 +1,4 @@
-function [vol,q,q_area,status]=CheckMesh3D(type,e,p,nodemap)
+function [vol,q,q_area,status]=CheckMesh3D(e,p,nodemap,input_flags)
 % CheckMesh3D(type,e,p,nodemap) checks the integrity of a 3D mesh
 % User can provide list of elements and points or provide a filename that
 % contains the mesh. If 'type' is zero then he will be prompted for a
@@ -17,9 +17,23 @@ function [vol,q,q_area,status]=CheckMesh3D(type,e,p,nodemap)
 % 2: do a mesh quality only
 % 3: only check if there is any void element
 % Return valuse: (status is a 'struct')
-% 1: Provided closed surface is not valid!
-% 2: Provided surface is multiple material or an invalid single material!
-% 3: Provided surface has faces with low quality < q_area_threshold
+% 1: Provided surface is multiple material
+% 2: Provided closed surface is not valid! 
+% 3: surface is both multip material and open
+% 4: Provided surface has faces with low quality < q_area_threshold
+
+if nargin>3 && ~isempty(input_flags)
+    if isfield(input_flags,'type')
+        type = input_flags.type;
+    else
+        type=0;
+    end
+    if isfield(input_flags,'verbose')
+        verbose=input_flags.verbose;
+    else
+        verbose=0;
+    end
+end
 vol=0;
 q=0;
 q_area=0;
@@ -45,23 +59,23 @@ else
 end
 if type==0
     if nnpe==3
-        disp([fn ' should be a surface mesh.']);
-        [q,q_area,area,zeroflag,edges,edgecnt,edgeIndexing,edgeflag]=checkmesh3d_surface(e,p,0,nodemap);
+        if verbose, disp([fn ' should be a surface mesh.']); end
+        [q,q_area,area,zeroflag,edges,edgeflag]=checkmesh3d_surface(e,p,nodemap);
     else
-        disp([fn ' should be a solid mesh.']);
+        if verbose, disp([fn ' should be a solid mesh.']); end
         [vol,q,zeroflag]=checkmesh3d_solid(e,p,1,nodemap);
     end
 else
     nnpe=size(e,2);
     if nnpe==3
-        disp('Checking surface mesh:');
-        if nargin==4
-            [q,q_area,area,zeroflag,edges,edgecnt,edgeIndexing,edgeflag]=checkmesh3d_surface(e,p,0,nodemap);
+        if verbose, disp('Checking surface mesh:');end
+        if nargin==4 && ~isempty(nodemap)
+            [q,q_area,area,zeroflag,edges,edgeflag]=checkmesh3d_surface(e,p,nodemap);
         else
-            [q,q_area,area,zeroflag,edges,edgecnt,edgeIndexing,edgeflag]=checkmesh3d_surface(e,p,0);
+            [q,q_area,area,zeroflag,edges,edgeflag]=checkmesh3d_surface(e,p);
         end
     else
-        disp('Checking solid mesh:');
+        if verbose, disp('Checking solid mesh:');end
         if nargin==4
             [vol,q,zeroflag]=checkmesh3d_solid(e,p,1,nodemap);
         else
@@ -73,24 +87,27 @@ if nnpe==3
     if edgeflag~=0
         switch edgeflag
             case 1
-                status.a=1;
-                disp('Provided surface is not valid!');
-                disp(['Please check all the diagnostic results stored in ' fn '-.txt files']);
+                status.b=1;
+                if verbose, 
+                    cprintf([1 0.4 0.1],'\n Some of mesh edges are shared by more than two triangles! (multiple material mesh ?!)\n');
+                    cprintf([1 0.4 0.1],' Please check all the diagnostic results stored in %s-.txt files.\n',fn);
+                end
             case 2
                 status.b=2;
-                cprintf([1 0.4 0.1], '  Provided surface is not closed:\n');
-                cprintf([0.1 0.5 0.9], '  At least one of the edges is only shared by only one triagnle (it should be two, at least)\n');
+                if verbose, 
+                    cprintf([1 0.4 0.1], '\n  Provided surface is not closed:\n');
+                    cprintf([0.1 0.5 0.9], '  At least one of the edges is only shared by only one triagnle (it should be two, at least)\n');
+                end
             otherwise
-                disp('CheckMesh3D result not implemented yet!');
+                if verbose, cprintf([1 0.4 0.1], '\n Surface mesh is open AND has edges shared by more than 2 triangles!\n');end
+                status.b=3;
         end
     end
     qcheck=q_area<q_area_threshold;
     a=sum(qcheck);
     if a~=0
-        disp(['There are ' num2str(a) ' faces that have a very low quality (q<' num2str(q_area_threshold) ').']);
-        disp('Should this mesh be used in a tetrahedral mesh generation, it will cause problems in');
-        disp('enhancing the mesh or trying to recover the original physical boundary surfaces');
-        status.c=3;
+        if verbose, cprintf([1 0.4 0.1],'There are %d faces that have a very low quality (q<%f).\n', a, q_area_threshold);end
+        status.b=4;
     end
 elseif nnpe==4
     if zeroflag~=0
@@ -98,47 +115,46 @@ elseif nnpe==4
     qcheck=q<TetrahedronFailQuality;
     a=sum(qcheck);
     if a~=0
-        disp(['There are ' num2str(a) ' tets that have a very low quality (q<' num2str(TetrahedronFailQuality) ').']);
-        status.c=3;
+        if verbose, cprintf([1 0.4 0.1],'There are %d tetrahedrons that have a very low quality (q<%f).\n', a, TetrahedronFailQuality);end
+        status.b=4;
     end
 end
 
-fn1=[fn '-quality-radius.txt'];
-fn2=[fn '-area.txt'];
-fn3=[fn '-edges.txt'];
-fn4=[fn '-edge-conn.txt'];
-fn5=[fn '-volume.txt'];
-os=computer;
-if ~isempty(strfind(os,'PCWIN')) % Windows
-    newlinech ='pc';
-elseif ~isempty(strfind(os,'MAC')) ||  ~isempty(strfind(os,'GLNX86')) % Mac OS or Linux
-    newlinech ='unix';
-end
-fprintf('\n');
-if ~isempty(q) && nnpe==3
-    cprintf('-comment',' Avg Quality (radius ratio): %f\n', mean(q));
-    cprintf('-comment',' Avg Quality (area   ratio): %f\n', mean(q_area));
-    dlmwrite(fn1,q,'delimiter',' ','newline',newlinech);
-    fn1=[fn '-quality-area.txt'];
-    dlmwrite(fn1,q_area,'delimiter',' ','newline',newlinech);
-end
-if nnpe==3 && ~isempty(area)
-    cprintf('-comment',' Avg Area: %f\n', mean(area));
-    dlmwrite(fn2,area,'delimiter',' ','newline',newlinech);
-end
-if nnpe==3 && ~isempty(edges)
-    dlmwrite(fn3,edges,'delimiter',' ','newline',newlinech);
-end
-if nnpe==3 && ~isempty(edgecnt)
-    dlmwrite(fn4,edgecnt,'delimiter',' ','newline',newlinech);
-end
-if nnpe==4 && ~isempty(vol)
-    [v,idx]=sort(vol);
-    dlmwrite(fn5,[idx v],'delimiter',' ','newline',newlinech);
-end
-if nnpe==4 && ~isempty(q)
-    cprintf('-comment',' Avg Quality (vol ratio): %f\n', mean(q));
-    dlmwrite([fn 'quality-vol-ratio.txt'],q,'delimiter',' ','newline',newlinech);
+if isfield(input_flags,'writefiles') && input_flags.writefiles==1
+    fn1=[fn '-quality-radius.txt'];
+    fn2=[fn '-area.txt'];
+    fn3=[fn '-edges.txt'];
+    fn4=[fn '-edge-conn.txt'];
+    fn5=[fn '-volume.txt'];
+    os=computer;
+    if ~isempty(strfind(os,'PCWIN')) % Windows
+        newlinech ='pc';
+    elseif ~isempty(strfind(os,'MAC')) ||  ~isempty(strfind(os,'GLNX86')) % Mac OS or Linux
+        newlinech ='unix';
+    end
+    fprintf('\n');
+    if ~isempty(q) && nnpe==3
+        cprintf('-comment',' Avg Quality (radius ratio): %f\n', mean(q));
+        cprintf('-comment',' Avg Quality (area   ratio): %f\n', mean(q_area));
+        dlmwrite(fn1,q,'delimiter',' ','newline',newlinech);
+        fn1=[fn '-quality-area.txt'];
+        dlmwrite(fn1,q_area,'delimiter',' ','newline',newlinech);
+    end
+    if nnpe==3 && ~isempty(area)
+        cprintf('-comment',' Avg Area: %f\n', mean(area));
+        dlmwrite(fn2,area,'delimiter',' ','newline',newlinech);
+    end
+    if nnpe==3 && ~isempty(edges)
+        dlmwrite(fn3,edges,'delimiter',' ','newline',newlinech);
+    end
+    if nnpe==4 && ~isempty(vol)
+        [v,idx]=sort(vol);
+        dlmwrite(fn5,[idx v],'delimiter',' ','newline',newlinech);
+    end
+    if nnpe==4 && ~isempty(q)
+        cprintf('-comment',' Avg Quality (vol ratio): %f\n', mean(q));
+        dlmwrite([fn 'quality-vol-ratio.txt'],q,'delimiter',' ','newline',newlinech);
+    end
 end
     
 
---
+++ base/trunk/checkedges.m
@@ -0,0 +1,37 @@
+function [retflag, edges] = checkedges(e)
+% retflag=0 no problem
+% retflag=1 3d mesh might be multiple material or invalid
+% retfalg=2 3d mesh is invalid (or at least open)
+% retflag=3 both open surface and multi-material
+
+retflag=0;
+edges = [e(:,[1 2]);e(:,[1 3]);e(:,[2 3])];
+[edges ix jx] = unique(sort(edges,2),'rows');
+vec=histc(jx,1:max(jx));
+
+b = vec==1; 
+c = vec>2;
+sumb=sum(b); sumc=sum(c);
+
+
+os=computer;
+if ~isempty(strfind(os,'PCWIN')) % Windows
+    newlinech ='pc';
+elseif ~isempty(strfind(os,'MAC')) ||  ~isempty(strfind(os,'GLNX86')) % Mac OS or Linux
+    newlinech ='unix';
+end
+
+
+if sumb~=0 % Some edges are used only once.
+    dlmwrite('SurfaceMesh-InvalidEdges.txt',edges(jx(b),:),'delimiter',' ','newline',newlinech);
+    retflag=2;
+end
+if sumc~=0 % Some esges are used more than twice
+    dlmwrite('SurfaceMesh-InvalidEdges.txt',edges(jx(c),:),'delimiter',' ','newline',newlinech);
+    if retflag==2
+        retflag=3;
+    else
+        retflag=1;
+    end
+end
+
