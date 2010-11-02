6d6ce4b15fb953b318d428b60299fe32b21a4884
hamiid <hamiid@fb8e7f84-bfd6-7a22-e4a7-981ca257bf3e>
2010-11-02 21:12:34 +0000

    Fixed a bug in writeOFF.m

--- base/trunk/writeOFF.m
+++ base/trunk/writeOFF.m
@@ -1,13 +1,18 @@
 function writeOFF(fn,t,p)
 % Writes surface mesh defined in 't' and 'p' to a filed called fn
 % Note: At the momemnt it assumes mesh is a triangulated surface mesh only
+% http://shape.cs.princeton.edu/benchmark/documentation/off_format.html
+%
+% Written by:
+%           Hamid Ghadyani Oct 2010
 
 fid = OpenFile(fn,'wt');
 
-fprintf(fid,'%d %d 0\n',size(t,1),size(p,1));
-
-fprintf(fid,'%d %d %d\n',(t(:,1:3)-1)');
+fprintf(fid,'OFF\n%d %d 0\n',size(p,1),size(t,1));
 
 fprintf(fid,'%.16f %.16f %.16f\n',(p(:,1:3))');
+
+fprintf(fid,'3 %d %d %d\n',[ones(size(t,1),1)*3 t(:,1:3)-1]');
+
 
 fclose(fid);
