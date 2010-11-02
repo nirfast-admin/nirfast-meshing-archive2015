34cbf0b782813255c3e84b89a94a3d10d3383b8f
hamiid <hamiid@fb8e7f84-bfd6-7a22-e4a7-981ca257bf3e>
2010-11-02 22:04:21 +0000

    Fixed another bug in writeOFF.m!

--- base/trunk/writeOFF.m
+++ base/trunk/writeOFF.m
@@ -12,7 +12,7 @@ fprintf(fid,'OFF\n%d %d 0\n',size(p,1),size(t,1));
 
 fprintf(fid,'%.16f %.16f %.16f\n',(p(:,1:3))');
 
-fprintf(fid,'3 %d %d %d\n',[ones(size(t,1),1)*3 t(:,1:3)-1]');
+fprintf(fid,'3 %d %d %d\n',(t(:,1:3)-1)');
 
 
 fclose(fid);
