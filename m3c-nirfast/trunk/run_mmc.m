7209c3b4a3963eee2dee8cda422dd5497ec1a3b4
hamiid <hamiid@fb8e7f84-bfd6-7a22-e4a7-981ca257bf3e>
2012-06-11 14:03:26 +0000

    Fixed path/permissions issues.

--- m3c-nirfast/trunk/run_mmc.m
+++ m3c-nirfast/trunk/run_mmc.m
@@ -1,8 +1,11 @@
 function [e p] = run_mmc(img,nrow,ncol,nslice,xypixelsize,zpixelsize,edgesize,outputdir,outputfn)
 
+if ~canwrite(outputdir)
+    outputdir = tempdir;
+    disp(['No write access, writing here instead: ' [outputdir outputfn]]);
+end
 mypwd = pwd;
 cd(outputdir);
-
 
 props.dim = 3;
 props.mysize = [nrow ncol nslice];
@@ -13,6 +16,7 @@ props.datatype = 'WORD';
 props.endian = 'ieee-le';
 
 mmcfn = 'mmc_imgstack';
+m3ctempfn = 'm3cinput.txt';
 WriteSPRHeaderInfo(mmcfn,props);
 
 fid = fopen([mmcfn '.sdt'],'wb');
@@ -26,7 +30,7 @@ end
 fclose(fid);
 %% Run M3C
 % set up the input file to M3C
-fid = fopen('m3cinput.txt','wt');
+fid = fopen(m3ctempfn,'wt');
 fprintf(fid,'%s\n',outputdir);
 fprintf(fid,'%s\n',mmcfn);
 fprintf(fid,'0\n'); % Specify slice range ?
@@ -43,7 +47,7 @@ fprintf(fid,'%s\n',outputfn);
 fclose(fid);
 
 systemcommand=GetSystemCommand('m3c');
-m3c_cmd=['! "' systemcommand '" < m3cinput.txt > junk.txt'];
+m3c_cmd=['! "' systemcommand '" < ' m3ctempfn ' > ' tempdir 'm3cjunk.txt'];
 
 fprintf('\n----> Running Marching Cube, please wait...');
 eval(m3c_cmd);
@@ -56,7 +60,7 @@ movefile([outputfn '_tetgen.ele'],[outputdir filesep outputfn '.ele']);
 
 %% Delete extra files
 delfiles = {'tmp.elm','tmp.nod','mc_layer.elm','mc_layer.nod','layer.nod',...
-             'm3cinput.txt',[mmcfn '.sdt'],[mmcfn '.spr'],'junk.txt',...
+             'm3cinput.txt',[mmcfn '.sdt'],[mmcfn '.spr'],...
              [outputfn '_nod.dat'],[outputfn '_elm.dat'],...
              [outputfn '_*.vtk']};
 for i=1:length(delfiles)
