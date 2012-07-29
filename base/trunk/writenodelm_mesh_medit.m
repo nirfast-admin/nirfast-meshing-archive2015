59287302deefefa6143be6a3a94bcdb428b185c6
hamiid <hamiid@fb8e7f84-bfd6-7a22-e4a7-981ca257bf3e>
2012-07-29 13:42:43 +0000

    completely rewrote writenodelm_mesh_medit to support tet/hex/tri elements as well as writing solution files in medit .bb format so it can be visualized in medit.

--- base/trunk/writenodelm_mesh_medit.m
+++ base/trunk/writenodelm_mesh_medit.m
@@ -1,37 +1,49 @@
-function writenodelm_mesh_medit(fn,tet,p,nodemap)
+function writenodelm_mesh_medit(infn,elm,p,nodemap,nnpe)
 % writenodelm_mesh_medit(fn,tet,p)
 % Writes a tetrahedral mesh defined in 'tet' and 'p' to a file called 'fn'
 % in medit format (.mesh) so it can be viewed using medit.
 % Note that this routine renumbers the node numbers from 1 to N and accordingly
 % adjust the node numbers in mesh list.
 
-fprintf('%s','Writing data to file... ')
-ref=0;
-tet_mat=10;
+if isempty(infn), error('Filename should be specified'), end
+
 np=size(p,1); dim=size(p,2);
-ntet=size(tet,1);
-if size(tet,2)<3 && size(tet,2)~=3
-    disp('The provided mesh is not a tetrahedral or triangular mesh!');
-    error('writenodelm_mesh_medit() can only write tetrahedral/triangular mesh to medit format');
-end
-if dim~=3
+nelm=size(elm,1);
+if dim<3
     error('Input points are not 3D');
 end
-if isempty(fn), error('Filename should be specified'), end
 
-if size(tet,2)==5 % material list for a multiple material mesh
-    mmflag = true;
+k = size(elm,2);
+if nargin < 5 || isempty(nnpe) || nnpe == 0
+    if k >= 8
+        nnpe = 8;
+    elseif k>=4
+        nnpe = 4;
+    elseif k>=3
+        nnpe = 3;
+    else
+        error('writenodelm_mesh_medit: input element type not supported. (only tri/tet/hex)')
+    end
+end
+
+if k - nnpe > 0
+    mmflag = 1;
+    eID = elm(:,nnpe+1);
 else
-    mmflag = false;
+    mmflag = 0;
+    eID = ones(nelm,1);
 end
 
-os=computer;
-if ~isempty(strfind(os,'PCWIN')) % Windows
-    newlinech ='pc';
-elseif ~isempty(strfind(os,'MAC')) ||  ~isempty(strfind(os,'GLNX86')) % Mac OS or Linux
-    newlinech ='unix';
+if size(p,2) > 3
+    nsol = p(:,4:end);
 end
-if nargin==4
+
+
+fprintf('%s','Writing data to file... ')
+
+if nargin < 4 || isempty(nodemap)
+    nn=(1:np)';
+else
     if size(nodemap,1)==1
         nn=nodemap';
     elseif size(nodemap,2)==1
@@ -39,58 +51,62 @@ if nargin==4
     elseif size(nodemap,2)~=1
         error('The nodemap list provided should be an n by 1 vector');
     end
-    [mytflag newtets]=ismember(tet(:,1:4),nodemap);
-    if mmflag
-        newtets = [newtets tet(:,5)];
-    end
-    sum1=sum(mytflag,2);
-    sumbflag=sum1==4;
-    if sum(sumbflag)~=ntet % not all vertices of 'faces' are given in nodemap list!
-        disp(' ');
-        disp('writenodelm_surface_medit: Not all vertices of the input tet could be found in given nodemap list!')
-        error('writenodelm_surface_medit: tets and nodemap lists are not compatible!')
-    end
-%     newfaces=nodemap(faces);
-%     newfaces=faces;
-else
-    nn=(1:np)';
-    newtets=tet;
+end
+[mytflag newelm]=ismember(elm(:,1:nnpe),nn);
+sum1=sum(mytflag,2);
+sumbflag = sum1==nnpe;
+if sum(sumbflag)~=nelm % not all vertices of 'faces' are given in nodemap list!
+    disp(' ');
+    disp('writenodelm_surface_medit: Not all vertices of the input mesh could be found in given nodemap list!')
+    error('writenodelm_surface_medit: mesh and nodemap lists are not compatible!')
 end
 
+newelm = [newelm eID];
+
+fn = add_extension(infn,'.mesh');
 fid=fopen(fn,'wt');
 fprintf(fid,'MeshVersionFormatted 1\n');
 fprintf(fid,'Dimension\n3\nVertices\n%u\n',np);
 
-% fclose(fid);
-% dlmwrite(fn,[nn p],'precision',10,'delimiter',' ','-append','newline',newlinech);
-% for i=1:np
-%     fprintf(fid,'%.12f %.12f %.12f %d\n',p(i,1),p(i,2),p(i,3),ref);
-% end
 
-fprintf(fid,'%.12f %.12f %.12f %d\n',[p(:,1:3) ones(np,1)*ref]');
+fprintf(fid,'%.12f %.12f %.12f %d\n',[p(:,1:3) ones(np,1,'int8')]');
 
-% fid=fopen(fn,'at+');
-if size(tet,2)>=4
-    fprintf(fid,'Tetrahedra\n%u\n',ntet);
-elseif size(tet,2)==3
-    fprintf(fid,'Triangles\n%u\n',ntet);
+if nnpe == 4
+    ekeyword = 'Tetrahedra';
+elseif nnpe == 8
+    ekeyword = 'Hexahedra';
+elseif nnpe == 3
+    ekeyword = 'Triangles';
 end
-% fclose(fid);
-if mmflag
-%     dlmwrite(fn,newtets,'precision','%u','delimiter',' ','-append','newline',newlinech);
-    fprintf(fid,'%d %d %d %d %d\n',(newtets(:,1:5))');
-else
-%     dlmwrite(fn,[newtets ones(ntet,1)*tet_mat],'precision','%u','delimiter',' ','-append','newline',newlinech);
-    fprintf(fid,'%d %d %d %d %d\n',[newtets(:,1:4) ones(ntet,1)*tet_mat]');
-end
+fprintf(fid,'%s\n%u\n',ekeyword,nelm);
 
-% fid=fopen(fn,'at+');
+sf = repmat('%d ',1,nnpe);
+sf = [sf '%d\n'];
+
+fprintf(fid,sf,newelm');
+
 fprintf(fid,'End\n');
 fclose(fid);
 
+type = 0;
+if size(p,2) > 3
+    type = 2;
+    sol = nsol;
+elseif mmflag
+    type = 1;
+    sol = elm(:,9:end);
+end
+if type
+    write_medit_sol_file(infn,sol,type);
+end
+
 [mypath myfn myext]=fileparts(fn);
-fprintf('\b\b%s\n','Done writing mesh to:')
+fprintf('\b\b%s\n','Done writing mesh to:');
 if ~isempty(mypath)
     fprintf('\t%s\n',['Path: ' mypath])
 end
-fprintf('\t%s\n',['Filename: ' myfn myext])
+fprintf('\t%s\n',['Filename: ' myfn myext]);
+if type
+    fprintf('\t%s\n',['Solution: ' infn '.bb']);
+end
+
