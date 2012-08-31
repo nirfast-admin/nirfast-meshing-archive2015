d228ca69ce651bcc66a4c5d97951eca032bf73c9
hamiid <hamiid@fb8e7f84-bfd6-7a22-e4a7-981ca257bf3e>
2012-08-31 11:43:12 +0000

    Refactor GetAvgEdgeSize() function
    - It now also handles tetrahedral meshes.

--- checkerboard3dmm/trunk/checkerboard3d.m
+++ checkerboard3dmm/trunk/checkerboard3d.m
@@ -98,7 +98,7 @@ end
 
 % Until we implement a density function, we will use the average edge size
 % as the initial desired length.
-edgesize = GetAverageEdgeSize(e,p);
+edgesize = GetEdgeSize(e, p);
 
 if silentflag==0
     prompt='Please enter Average Desired Mesh Length:';
@@ -269,11 +269,4 @@ PP = interior_p0((st1==1),:);
 
 
 
-function edgesize = GetAverageEdgeSize(e,p)
-% Assuming e and p define a 3D shell surface, this function returns the
-% average edge size of the triangular patches.
 
-edges=[e(:,[1 2]); e(:,[1 3]); e(:,[2 3])];
-edges = unique(sort(edges,2),'rows');
-edgelength=sqrt(sum((p(edges(:,2),:)-p(edges(:,1),:)).^2,2));
-edgesize=mean(edgelength);
